3a3091c9bf8f66dcf83a4bcc7b0da7c369dbe811
Jonathan Marroquin <jhmcantu@gmail.com>
2017-02-24 23:18:44 -0800

    add db

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import sys
-import json
-
+import psycopg2
+import urlparse
 import requests
+
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -14,7 +15,23 @@ def ok():
 
 @app.route('/validate', methods=['GET'])
 def validate():
-    if request.args.get("cdkey") == "test-test-test-test":
+
+    urlparse.use_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+        )
+    
+    cur = conn.cursor()
+    cur.execute("Select 'test-test-test-test';")
+    r = cur.fetchone()
+    log(r)
+    if request.args.get("cdkey") == r[1]:
         return "ok",200
     return "Im a teapot", 418
 
